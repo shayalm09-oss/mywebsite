99643eba2e2bd01c0e5ec97af57f145b50822c12
shayalm09-oss <shayalm09@gmail.com>
2026-04-23 09:37:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.write("my superb website")
-st.header("the best website you will ever see")
-st.subheader("so what is this website...?")
-name = st.text_input("your name?")
+st.header("To Do List")
+name = st.text_input("name of task")
+prioraty= st.text_input("prioraty of task")
+time = st.text_input("time of task")
