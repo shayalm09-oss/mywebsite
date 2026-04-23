26ff98ad771141a5617f9f9bd65e53ed84798db7
shayalm09-oss <shayalm09@gmail.com>
2026-04-23 09:31:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
 import streamlit as st
-st.write("האתר שלי")
+
+# Define your color
+bg_color = "#c2c395"
+
+# Inject CSS with markdown
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-color: {bg_color};
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.write("my superb website")
+st.header("the best website you will ever see")
+st.subheader("so what is this website...?")
+name = st.text_input("your name?")
