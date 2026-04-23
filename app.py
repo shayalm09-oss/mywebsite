f5ab1f6e80b4f5762b32391181f3abe528aa307c
shayalm09-oss <shayalm09@gmail.com>
2026-04-23 21:06:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if 'done_count' not in st.session_state:
     st.session_state.done_count = 0
 
 # --- THE ORIGINAL COLORS ---
-bg_color = "#A0C4FF"     
+bg_color = "#9BF6FF"     
 title_color = "#27AE60"  
 card_color = "#9BF6FF"   
 
